add49dd5c0b480aa64e59fcc6558a83379db6e49
clarkalex <calexander@neit.edu>
2018-05-01 09:56:13 -0400

    Test with another controller class

---
+++ controllers/help.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calexander
+ * Date: 5/1/18
+ * Time: 9:53 AM
+ */
+class Help {
+    function __construct()
+    {
+        echo "we are in help";
+    }
+}
