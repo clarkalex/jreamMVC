1ff3ba7a044a2bf4532965854abcb8f0adb9f28b
clarkalex <calexander@neit.edu>
2018-05-01 10:00:01 -0400

    Explode the url and test

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
  * Date: 5/1/18
  * Time: 9:31 AM
  */
-$url = $_GET['url'];
-echo $url;
+
+$url = explode('/', $_GET['url']);
+print_r($url);
 require 'controllers/'. $url . '.php';
 $controller = new $url;
