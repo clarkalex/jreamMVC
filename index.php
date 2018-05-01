6f8289f72c114643154989ca7736f207c468eb1a
clarkalex <calexander@neit.edu>
2018-05-01 10:21:49 -0400

    Use a bootstrap and require it

--- index.php
+++ index.php
@@ -5,18 +5,5 @@
  * Date: 5/1/18
  * Time: 9:31 AM
  */
-$url = $_GET['url'];
-$url = rtrim($url, '/');
-$url = explode('/', $url);
-print_r($url);
-require 'controllers/'. $url[0] . '.php';
-$controller = new $url[0];
-
-if (isset($url[2])) {
-    $controller->{$url[1]}($url[2]); // $controller->function()
-} else {
-    if (isset($url[1])) {
-        $controller->{$url[1]}(); // $controller->function()
-    }
-}
-
+require 'libs/Bootstrap.php';
+$app = new Bootstrap();
---
+++ libs/Bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calexander
+ * Date: 5/1/18
+ * Time: 10:14 AM
+ */
+
+class Bootstrap {
+
+    function __construct()
+    {
+        $url = $_GET['url'];
+        $url = rtrim($url, '/');
+        $url = explode('/', $url);
+        print_r($url);
+        $file = 'controllers/'. $url[0] . '.php';
+        if (file_exists($file)){
+            require $file;
+        } else {
+            throw new Exception("The file $file Does not exist");
+        }
+
+        $controller = new $url[0];
+
+        if (isset($url[2])) {
+            $controller->{$url[1]}($url[2]); // $controller->function()
+        } else {
+            if (isset($url[1])) {
+                $controller->{$url[1]}(); // $controller->function()
+            }
+        }
+
+    }
+}
